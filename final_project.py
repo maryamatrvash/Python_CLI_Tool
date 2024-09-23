dd04f0204e74785f77e3f786046fc6131fd7b0e0
MaryamAtrvash <maryamatrvash83@gmail.com>
2024-09-23 22:10:55 +0330

    Edit setup & ls

--- final_project.py
+++ final_project.py
@@ -6,14 +6,14 @@ import os
 def setup():
     parser = argparse.ArgumentParser(description="Python CLI Tool for File Manipulation")
     parser.add_argument("--ls", type=str, help="List directory contents at path or the current directory if no path is given")
-    parser.add_argument("--cd", type=str, help="Change the working directory to path")
+    parser.add_argument("--cd", type=str, help="Change the working directory to path") 
     parser.add_argument("--mkdir",type=str, help="Create a new directory at path")
     parser.add_argument("--rmdir", type=str, help="Remove the directory at path if it is empty") 
     parser.add_argument("--rm", type=str, help="Remove the file specified by file")  
     parser.add_argument("--rm-r", type=str, help="Remove the directory at directory and its contents recursively")
-    parser.add_argument("--cp", type=str, help="Copy a file or directory from source to destination")
-    parser.add_argument("--mv", type=str, help="Move a file or directory from sourse to destination")
-    parser.add_argument("--find", type=str, help="Search for files or directories matching pattern starting from path")
+    parser.add_argument("--cp", type=str, nargs=2, help="Copy a file or directory from source to destination")
+    parser.add_argument("--mv", type=str, nargs=2, help="Move a file or directory from sourse to destination")
+    parser.add_argument("--find", type=str, nargs=2, help="Search for files or directories matching pattern starting from path")
     parser.add_argument("--cat", type=str, help="Output the contents of the file") 
     return parser 
 
@@ -25,14 +25,12 @@ def log_command(cmd):
         file.write(text)  
  
 def ls(): 
-    if arguments.ls != "":
-        path = arguments.ls
-        path = path.replace(" ", "")
-        for p, dir, file in os.walk(path):
-            for d in dir:
-                print(str(d))
-            for f in file:
-                print(str(f)) 
+    path = arguments.ls
+    for p, dir, file in os.walk(path): 
+        for d in dir:
+            print(str(d))
+        for f in file:
+            print(str(f)) 
 
 def cd():
     cwd = os.getcwd() 
